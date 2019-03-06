c6b772e0c6bf1d7e43bf64341a2959e2483c565e
maschloerning <schloegel.max@gmail.com>
2019-03-06 13:32:38 +0100

    bug solving

--- 3/demo_lucas_kanade.m
+++ 3/demo_lucas_kanade.m
@@ -1,9 +1,41 @@
 % read images synth1.pgm and synth2.pgm
-Vx_Vy = lucas_kanade('sphere1.ppm', 'sphere2.ppm');
+im1 = imread('synth2.pgm')
+im2 = imread('synth1.pgm')
+Vy_Vx = lucas_kanade(im1, im2);
+[h,w] = size(imread('synth1.pgm'));
+%display(Vx_Vy)
+size(Vy_Vx(:,:,1));
+h = floor(h/15) * 15;
+w = floor(w/15) * 15;
 
-display(Vx_Vy)
+[x,y] = meshgrid(8:15:h,8:15:w);
+
+%size([x,y]);
+
+
+figure
+quiver(x,y,Vy_Vx(:,:,1), Vy_Vx(:,:,2))
 
 % compute the optical flow using 'lucas_kanade.m'
+
+im1 = rgb2gray(imread('sphere2.ppm'));
+im2 = rgb2gray(imread('sphere1.ppm'));
+%imshow(im1)
+%imshow(im2)
+Vy_Vx = lucas_kanade(im1, im2)
+[h,w] = size(rgb2gray(imread('sphere1.ppm')));
+%display(Vx_Vy)
+size(Vy_Vx(:,:,1)');
+h = floor(h/15) * 15;
+w = floor(w/15) * 15;
+
+[x,y] = meshgrid(8:15:h,8:15:w);
+
+size([x,y]);
+
+
+figure
+quiver(y,x,Vy_Vx(:,:,2), Vy_Vx(:,:,1)')
 
 % plot the quiver on top of the original image
 
--- 3/lucas_kanade.m
+++ 3/lucas_kanade.m
@@ -1,6 +1,5 @@
-function [Vx_Vy] = lucas_kanade(image1, image2)
-im1 = imread(image1);
-im2 = imread(image2);
+function [Vy_Vx] = lucas_kanade(im1, im2)
+
 
 blocksize = 15;
 
@@ -13,18 +12,22 @@ It = zeros(size(im1));
 
 for i = 1:h
     for j = 1:w
-        [ix,iy,it] = getDerivative(i,j, im1, im2);
+        [iy,ix,it] = getDerivative(i,j, im1, im2);
         Ix(i,j) = ix;
         Iy(i,j) = iy;
         It(i,j) = it;
     end
 end
 
+%imshow(Ix)
+%imshow(Iy)
+%imshow(It)
+
 M_blocks_Iy = cell(block_rows,block_columns);
 M_blocks_Ix = cell(block_rows,block_columns);
 M_blocks_It = cell(block_rows,block_columns);
 
-Vx_Vy = zeros(block_rows,block_columns,2);
+Vy_Vx = zeros(block_rows,block_columns,2);
 
 for i = 1:block_rows
     for j = 1:block_columns
@@ -33,7 +36,7 @@ for i = 1:block_rows
     M_blocks_It{i,j} = It(1 + (i-1)*blocksize:blocksize * i, 1+ (j-1)*blocksize: blocksize*j);
     
     [A,b] =generateAb(M_blocks_Iy{i,j},M_blocks_Ix{i,j}, M_blocks_It{i,j});
-    Vx_Vy(i,j,:) = computeOpticalFlow(A,b);
+    Vy_Vx(i,j,:) = computeOpticalFlow(A,b);
     end
 end
 
@@ -58,18 +61,20 @@ end
 
 function [A,b] = generateAb(block_Iy, block_Ix, block_It)
 
-[h,w] = size(block_Iy)
+[h,w] = size(block_Iy);
 A = zeros(h*w,2);
 
 b = -block_It(:);
-A(:,1) = block_Ix(:);
-A(:,2) = block_Iy(:);
+A(:,2) = block_Ix(:);
+A(:,1) = block_Iy(:);
 
 end
 
-function [vx_vy] = computeOpticalFlow(A,b)
+function [vy_vx] = computeOpticalFlow(A,b)
 
-vx_vy = mtimes(inv(mtimes(A.',A)), mtimes(A.',b));
+[vy_vx,~] = linsolve(mtimes(A.',A), mtimes(A.',b));
+%vx_vy = mtimes(inv(mtimes(A.',A)), mtimes(A.',b));
+
 
 
 end
