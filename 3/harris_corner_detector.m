302847259dab6e6b2e7e9e4a3708a046a8c56772
ancadiana23 <ancadianavicol@gmail.com>
2019-03-06 17:55:45 +0100

    tunning

--- 3/harris_corner_detector.m
+++ 3/harris_corner_detector.m
@@ -4,16 +4,16 @@ sigma = 0.9;
 
 I = rgb2gray(image);
 I = double(I);
-%I = imgaussfilt(I, sigma);
+I = imgaussfilt(I, 0.7);
 
-gauss_kernel = fspecial('gaussian', [5, 5], 0.6);
+gauss_kernel = fspecial('gaussian', [3, 3], 0.6);
 [Gx,Gy] = gradient(gauss_kernel);
 
 %Gy = [1 0 -1; 2 0 -2; 1 0 -1];
 %Gx = [1 2 1; 0 0 0; -1 -2 -1];
 
-Ix = conv2(I, Gx);
-Iy = conv2(I, Gy);
+Ix = imfilter(I, Gx, 'replicate', 'same', 'conv');
+Iy = imfilter(I, Gy, 'replicate', 'same', 'conv');
 
 figure, imshow(uint8(Ix));
 figure, imshow(uint8(Iy));
@@ -23,13 +23,12 @@ A = imgaussfilt(Ix .^ 2, sigma);
 B = imgaussfilt(Ix .* Iy, sigma);
 C = imgaussfilt(Iy .^ 2, sigma);
 
-H = (A .* C - B.^2 ) - 0.04 * (A + C).^2;
+H = (A .* C - B.^2 ) - 0.04 * ((A + C).^2);
 H = H ./ max(max(H));
-
-%corner_points = imregionalmax(H,8);
-%corner_points = H .* corner_points;
-
-
+size_H = size(H)
+corner_points = imregionalmax(H,8);
+corner_points = H .* corner_points;
+%{
 [h, w] = size(H);
 window_size = 7;
 offset = floor(window_size / 2)
@@ -44,11 +43,10 @@ for i = 1:h
         corner_points(i, j) = corner_points(i, j) * H(i, j);
     end 
 end
+%}
 
-
-threshold = 50 * mean(mean(corner_points))
-%threshold = 0.0005
-
+threshold = 100 * mean(mean(corner_points))
+size_corner = size(corner_points)
 [r, c] = find(corner_points > threshold);
 
 figure, imshow(image);
--- 3/test.m
+++ 3/test.m
@@ -1,27 +1,26 @@
+fprintf("ping-pong \n")
 image = imread('./pingpong/0000.jpeg');
-[H, r, c] = harris_corner_detector(image);
+[H1, r, c] = harris_corner_detector(image);
 
+fprintf("toy \n")
 image = imread('./person_toy/00000001.jpg');
-[H, r_0, c_0] = harris_corner_detector(image);
+[H2, r_0, c_0] = harris_corner_detector(image);
 
+fprintf("toy 45 \n")
 I_45 = imrotate(image, 45);
-[H, r_45, c_45] = harris_corner_detector(I_45);
+[H3, r_45, c_45] = harris_corner_detector(I_45);
 
+fprintf("toy 90 \n")
 I_90 = imrotate(image, 90);
-[H, r_90, c_90] = harris_corner_detector(I_90);
+[H4, r_90, c_90] = harris_corner_detector(I_90);
 
-[h, w, channels] = size(image)
-size(I_45)
-size(I_90)
+[h, w, channels] = size(image);
 
-fprintf("diff")
-sum(sum(r_0 ~= c_90))
-sum(sum(c_0 ~= w - r_90))
-
-figure, imshow(image)
+% it's interesting that the outputs are almost the same just a bit shifted
+figure, imshow(image);
 hold on
 plot(c_0, r_0, 'o');
-plot(h - r_90, c_90, 'o');
+plot(w - r_90 + 1, c_90, 'o');
 
 hold off
 drawnow
