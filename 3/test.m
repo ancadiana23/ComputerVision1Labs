19ea4deaa5023233e650c4db8067d1f46ea39662
ancadiana23 <ancadianavicol@gmail.com>
2019-03-06 03:00:51 +0100

    ex 1 plot sync

--- 3/test.m
+++ 3/test.m
@@ -1,7 +1,12 @@
 image = imread('./pingpong/0000.jpeg');
 [H, r, c] = harris_corner_detector(image);
 
-imshow(image);
-hold on
-plot(c, r, 'o');
-hold off
+image = imread('./person_toy/00000001.jpg');
+[H, r, c] = harris_corner_detector(image);
+
+
+I_45 = imrotate(image, 45);
+[H, r, c] = harris_corner_detector(I_45);
+
+I_90 = imrotate(image, 90);
+[H, r, c] = harris_corner_detector(I_90);
