37fd225b9d6a84695b60fc0a845809f32bfa6f26
ancadiana23 <ancadianavicol@gmail.com>
2019-03-06 12:51:22 +0100

    regional max window

--- 3/harris_corner_detector.m
+++ 3/harris_corner_detector.m
@@ -7,9 +7,9 @@ I = rgb2gray(image);
 Ix = imfilter(I, Gx);
 Iy = imfilter(I, Gy);
 
-figure, imshow(Ix);
-figure, imshow(Iy);
-drawnow
+%figure, imshow(Ix);
+%figure, imshow(Iy);
+%drawnow
 
 A = imgaussfilt(Ix .^ 2);
 B = imgaussfilt(Ix .* Iy);
@@ -23,11 +23,18 @@ threshold = 220;
 %corner_points = double(H) .* double(corner_points);
 
 [h, w] = size(H);
+window_size = 5;
+offset = floor(window_size / 2)
 new_corner_points = zeros(h, w);
-padded_H = padarray(H, [1, 1]);
+padded_H = padarray(H, [offset, offset]);
+size(H)
+size(padded_H)
 for i = 1:h
     for j = 1:w
-        new_corner_points(i, j) = sum(sum(padded_H(i:i+2, j:j+2) > padded_H(i + 1, j + 1))) == 0;
+        i2 = i + window_size - 1;
+        j2 = j + window_size - 1;
+        
+        new_corner_points(i, j) = sum(sum(padded_H(i:i2, j:j2) > H(i, j))) == 0;
         new_corner_points(i, j) = new_corner_points(i, j) * padded_H(i + 1, j + 1);
     end 
 end
