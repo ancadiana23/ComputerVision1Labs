87bfcc9109b72cdbccb74651bc75d09175a69b54
Benjamin Kolb <service_websites@gmx-ist-cool.de>
2019-03-07 19:46:05 +0100

    added vlfeat library

--- 4/RANSAC.m
+++ 4/RANSAC.m
@@ -9,64 +9,52 @@ Ib = imread('boat2.pgm');
 
 
 
-
-
-i=1;
-
-% calculate parameters
-x1 = matched_a(1,1)
-y1 = matched_a(2,1)
-x2 = matched_b(1,1)
-y2 = matched_b(2,1)
-
-A = [x1 y1 0 0 1 0; 0 0 x1 y1 0 1]
-b = [x2;y2]
-params = pinv(A)*b
-
 % this will be the RANSAC implementation
-% P = 25 % should become function input
-% n_matches = size(matched_a,2)
-% perm = randperm(n_matches) ;
-% sel = perm(1:P) 
-% for i in sel
-%     %compute params as above
-% end
+P = 25 % should become function input
+n_matches = size(matched_a,2)
+perm = randperm(n_matches) ;
+
+first=1 % tells if we are in the first iteration
+for i = perm(1:P)
+    x1 = matched_a(1,i);
+    y1 = matched_a(2,i);
+    x2 = matched_b(1,i);
+    y2 = matched_b(2,i);
+
+    A = [x1 y1 0 0 1 0; 0 0 x1 y1 0 1];
+    b = [x2;y2];
+    params = pinv(A)*b;
+    
+    n_inliers=0
+    paramA = [params(1) params(2); params(3) params(4)]
+    paramb = [params(5) ; params(6)]
+    for j = 1:n_matches
+        x1 = matched_a(1,j)
+        y1 = matched_a(2,j)
+        x2 = matched_b(1,j)
+        y2 = matched_b(2,j)
+        deviation = paramA*[x1;y1] + paramb - [x2;y2]
+        if norm(deviation)<=10
+            n_inliers=n_inliers+1
+        end
+    end
+    n_inliers/n_matches %print success rate
+    if first==1 
+        best_n_inliers=n_inliers
+        best_paramA=paramA
+        best_paramb=paramb
+        first=0
+    end
+    if n_inliers>best_n_inliers 
+        best_n_inliers=n_inliers
+        best_paramA=paramA
+        best_paramb=paramb
+    end
+
+    
+end
 
 
 
-% % plotting matched keypoints
-% imshow(Ia);
-% I=Ia;
-% h1 = vl_plotframe(matched_a) ;
-% h2 = vl_plotframe(matched_a) ;
-% set(h1,'color','k','linewidth',3) ;
-% set(h2,'color','y','linewidth',2) ;
 
-% % testing threshholds
-% imshow(Ia)
-% I=Ia;
-% peak_thresh = 20
-% [f,d] = vl_sift(I,'PeakThresh', peak_thresh) ;
-% % [f,d] = vl_sift(I) ;
-% h1 = vl_plotframe(f(:,:)) ;
-% h2 = vl_plotframe(f(:,:)) ;
-% set(h1,'color','k','linewidth',3) ;
-% set(h2,'color','y','linewidth',2) ;
-
-
-
-
-% % the following lines are just the illustration from the websites
-%I = vl_impattern('roofs1') ;
-%image(I);
-%I = single(rgb2gray(I)) ;
-%[f,d] = vl_sift(I) ;
-%perm = randperm(size(f,2)) ;
-%sel = perm(1:50) ;
-%h1 = vl_plotframe(f(:,sel)) ;
-%h2 = vl_plotframe(f(:,sel)) ;
-%set(h1,'color','k','linewidth',3) ;
-%set(h2,'color','y','linewidth',2) ;
-% h3 = vl_plotsiftdescriptor(d(:,sel),f(:,sel)) ;
-% set(h3,'color','g') ;
 
