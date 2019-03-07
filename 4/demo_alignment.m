592cf3276bb154407f36a5d7774e8110808ced3f
maschloerning <schloegel.max@gmail.com>
2019-03-08 00:54:51 +0100

    added transformation demo

--- 4/demo_alignment.m
+++ 4/demo_alignment.m
@@ -15,3 +15,27 @@ for iteration = 2:10
 end
 avg_success_rates=sum_success_rates/10;
 plot(1:size(avg_success_rates,2),avg_success_rates);
+
+
+% Demo for the image trasformation
+
+P = 25
+N = 500
+% transforming image2 so that it fits image1
+Ia = imread('boat1.pgm');
+Ib = imread('boat2.pgm');
+
+transformed_image = transform_image(Ia, Ib, P, N);
+
+figure
+imshow(transformed_image);
+
+% transforming image1 so that it fits image2
+Ia = imread('boat2.pgm');
+Ib = imread('boat1.pgm');
+
+transformed_image = transform_image(Ia, Ib, P, N);
+
+figure
+imshow(transformed_image);
+
