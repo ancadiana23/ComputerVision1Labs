806dc8082a916b0d8e9ade3572a28d12f89dfe52
maschloerning <schloegel.max@gmail.com>
2019-03-07 22:05:22 +0100

    debugging

--- 4/RANSAC.m
+++ 4/RANSAC.m
@@ -4,24 +4,30 @@
 Ia = imread('boat1.pgm');
 Ib = imread('boat2.pgm');
 
-P = 25 % should become function input
-N = 100
+P = 25; % should become function input
+N = 400;
 
 
 [matched_a,matched_b] = keypoint_matching(Ia,Ib);
 
-n_matches = size(matched_a,2)
+n_matches = size(matched_a,2);
 perm = randperm(n_matches) ;
 
 % initialize matrix that stores the best inliers in columns as emtpy matrix
 % -> (2, #best inliers) = size(inlier_matrix)
 % it saves the coordinates of transformed points of image2
 % that now lie in image1
-best_inlier_matrix = []
+best_inlier_matrix = [];
+best_n_inliers= 0;
+best_paramA= [];
+best_paramb= [];
+
+
 for j = 1:N
-    current_best_inlier_matrix = []
-    A = zeros(2*P, 6)
-    b = zeros(2*P, 1)
+    perm = randperm(n_matches) ;
+    current_best_inlier_matrix = [];
+    A = zeros(2*P, 6);
+    b = zeros(2*P, 1);
     for i=1:perm(1:P)
         x1 = matched_a(1,i);
         y1 = matched_a(2,i);
@@ -46,23 +52,27 @@ for j = 1:N
         deviation = paramA*[x1;y1] + paramb - [x2;y2];
         if norm(deviation)<=10
             n_inliers=n_inliers+1;
-            current_best_inlier_matrix = [current_best_inlier_matrix.'; paramA*[x1;y1] + paramb].'
+            current_best_inlier_matrix = [current_best_inlier_matrix.'; paramA*[x1;y1] + paramb].';
         end
     end
-    n_inliers/n_matches %print success rate
+    n_inliers/n_matches; %print success rate
     if j==1 
         best_n_inliers=n_inliers;
         best_paramA=paramA;
         best_paramb=paramb;
-        best_inlier_matrix = current_best_inlier_matrix
+        best_inlier_matrix = current_best_inlier_matrix;
     end
     if n_inliers>best_n_inliers 
         best_n_inliers=n_inliers;
         best_paramA=paramA;
         best_paramb=paramb;
-        best_inlier_matrix = current_best_inlier_matrix
+        best_inlier_matrix = current_best_inlier_matrix;
     end
 end
+
+best_n_inliers/n_matches
+best_paramA
+best_paramb
 
 
 
