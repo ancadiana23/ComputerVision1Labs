736a0cf86c32319e819dae19036ec8d025e2cdb3
Benjamin Kolb <service_websites@gmx-ist-cool.de>
2019-03-07 18:09:53 +0100

    added vlfeat library

--- 4/keypoint_matching.m
+++ 4/keypoint_matching.m
@@ -1,30 +1,24 @@
+function [matched_a,matched_b] = keypoint_matching(Ia,Ib)
+
 run('vlfeat-0.9.21/toolbox/vl_setup') %one-time setup
 
-Ia = imread('boat1.pgm');
-Ib = imread('boat2.pgm');
 Ia = single(Ia);
 Ib = single(Ib);
 
-h3 = vl_plotsiftdescriptor(d(:,sel),f(:,sel)) ;
-set(h3,'color','g') ;
-
 [fa, da] = vl_sift(Ia) ;
 [fb, db] = vl_sift(Ib) ;
+
+% if we want to include a threshold peak_thresh as function input:
+%[fa, da] = vl_sift(Ia, 'PeakThresh', peak_thresh) ;
+%[fb, db] = vl_sift(Ib, 'PeakThresh', peak_thresh) ;
+
 [matches, scores] = vl_ubcmatch(da, db);
 
+matched_a=fa(:,matches)
+matched_b=fa(:,matches)
 
+end
 
-% the following lines are just the illustration from the websites
-I = vl_impattern('roofs1') ;
-image(I);
-I = single(rgb2gray(I)) ;
-[f,d] = vl_sift(I) ;
-perm = randperm(size(f,2)) ;
-sel = perm(1:50) ;
-h1 = vl_plotframe(f(:,sel)) ;
-h2 = vl_plotframe(f(:,sel)) ;
-set(h1,'color','k','linewidth',3) ;
-set(h2,'color','y','linewidth',2) ;
 
 
 
