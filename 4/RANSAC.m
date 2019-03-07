ae0177058ff2c6da9f86b7a74874283dc2526980
Benjamin Kolb <service_websites@gmx-ist-cool.de>
2019-03-07 19:53:42 +0100

    added vlfeat library

--- 4/RANSAC.m
+++ 4/RANSAC.m
@@ -4,13 +4,11 @@
 Ia = imread('boat1.pgm');
 Ib = imread('boat2.pgm');
 
+P = 25 % should become function input
+
 
 [matched_a,matched_b] = keypoint_matching(Ia,Ib);
 
-
-
-% this will be the RANSAC implementation
-P = 25 % should become function input
 n_matches = size(matched_a,2)
 perm = randperm(n_matches) ;
 
@@ -25,30 +23,30 @@ for i = perm(1:P)
     b = [x2;y2];
     params = pinv(A)*b;
     
-    n_inliers=0
-    paramA = [params(1) params(2); params(3) params(4)]
-    paramb = [params(5) ; params(6)]
+    n_inliers=0;
+    paramA = [params(1) params(2); params(3) params(4)];
+    paramb = [params(5) ; params(6)];
     for j = 1:n_matches
-        x1 = matched_a(1,j)
-        y1 = matched_a(2,j)
-        x2 = matched_b(1,j)
-        y2 = matched_b(2,j)
-        deviation = paramA*[x1;y1] + paramb - [x2;y2]
+        x1 = matched_a(1,j);
+        y1 = matched_a(2,j);
+        x2 = matched_b(1,j);
+        y2 = matched_b(2,j);
+        deviation = paramA*[x1;y1] + paramb - [x2;y2];
         if norm(deviation)<=10
-            n_inliers=n_inliers+1
+            n_inliers=n_inliers+1;
         end
     end
     n_inliers/n_matches %print success rate
     if first==1 
-        best_n_inliers=n_inliers
-        best_paramA=paramA
-        best_paramb=paramb
-        first=0
+        best_n_inliers=n_inliers;
+        best_paramA=paramA;
+        best_paramb=paramb;
+        first=0;
     end
     if n_inliers>best_n_inliers 
-        best_n_inliers=n_inliers
-        best_paramA=paramA
-        best_paramb=paramb
+        best_n_inliers=n_inliers;
+        best_paramA=paramA;
+        best_paramb=paramb;
     end
 
     
