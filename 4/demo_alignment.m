f1103506d4915f8257dac471dc5123a184b3a1ec
Benjamin Kolb <service_websites@gmx-ist-cool.de>
2019-03-08 01:28:09 +0100

    minor chamges

--- 4/demo_alignment.m
+++ 4/demo_alignment.m
@@ -1,41 +1,37 @@
+P = 3;
+N = 100;
+
 Ia = imread('boat1.pgm');
 Ib = imread('boat2.pgm');
 
+% Demo for matched keypoints
+figure
 plot_matches(Ia,Ib);
 
-[best_paramA, best_paramb, best_inlier_matrix, success_rates] = RANSAC(Ia,Ib,3,100);
+% Demo for the image transformation
+
+% transforming image2 so that it fits image1
+transformed_image = transform_image(Ia, Ib, P, N);
+figure
+imshow(transformed_image);
+
+% transforming image1 so that it fits image2
+transformed_image = transform_image(Ib, Ia, P, N);
+figure
+imshow(transformed_image);
 
 
-
-% figuring out how many iterations we need
+% Plot for figuring out how many iterations we need
+[best_paramA, best_paramb, best_inlier_matrix, success_rates] = RANSAC(Ia,Ib,P,N);
 sum_success_rates = success_rates
 for iteration = 2:10
     [best_paramA, best_paramb, best_inlier_matrix, success_rates] = RANSAC(Ia,Ib,3,100);
     sum_success_rates = sum_success_rates + success_rates;
 end
 avg_success_rates=sum_success_rates/10;
+figure
 plot(1:size(avg_success_rates,2),avg_success_rates);
 
 
-% Demo for the image trasformation
 
-P = 25
-N = 500
-% transforming image2 so that it fits image1
-Ia = imread('boat1.pgm');
-Ib = imread('boat2.pgm');
-
-transformed_image = transform_image(Ia, Ib, P, N);
-
-figure
-imshow(transformed_image);
-
-% transforming image1 so that it fits image2
-Ia = imread('boat2.pgm');
-Ib = imread('boat1.pgm');
-
-transformed_image = transform_image(Ia, Ib, P, N);
-
-figure
-imshow(transformed_image);
 
--- 4/plot_matches.m
+++ 4/plot_matches.m
@@ -1,11 +1,10 @@
 function [] = plot_matches(Ia,Ib)
+    n_lines=10;
+
     Iab = [Ia Ib];
 
     [matched_a,matched_b] = keypoint_matching(Ia,Ib);
-
-    n_lines=10;
-
-
+    
     imshow(Iab);
     n_matches = size(matched_a,2);
     perm = randperm(n_matches);
@@ -21,38 +20,3 @@ function [] = plot_matches(Ia,Ib)
         line([matched_a(1,match),shift+matched_b(1,match)],[matched_a(2,match),matched_b(2,match)],'Color',[r g b],'LineWidth',2);
     end
  end
-
-
-% % plotting matched keypoints
-% imshow(Ia);
-% I=Ia;
-% h1 = vl_plotframe(matched_a) ;
-% h2 = vl_plotframe(matched_a) ;
-% set(h1,'color','k','linewidth',3) ;
-% set(h2,'color','y','linewidth',2) ;
-
-% % testing threshholds
-% imshow(Ia)
-% I=Ia;
-% peak_thresh = 20
-% [f,d] = vl_sift(I,'PeakThresh', peak_thresh) ;
-% % [f,d] = vl_sift(I) ;
-% h1 = vl_plotframe(f(:,:)) ;
-% h2 = vl_plotframe(f(:,:)) ;
-% set(h1,'color','k','linewidth',3) ;
-% set(h2,'color','y','linewidth',2) ;
-
-
-% % the following lines are just the illustration from the websites
-%I = vl_impattern('roofs1') ;
-%image(I);
-%I = single(rgb2gray(I)) ;
-%[f,d] = vl_sift(I) ;
-%perm = randperm(size(f,2)) ;
-%sel = perm(1:50) ;
-%h1 = vl_plotframe(f(:,sel)) ;
-%h2 = vl_plotframe(f(:,sel)) ;
-%set(h1,'color','k','linewidth',3) ;
-%set(h2,'color','y','linewidth',2) ;
-% h3 = vl_plotsiftdescriptor(d(:,sel),f(:,sel)) ;
-% set(h3,'color','g') ;
