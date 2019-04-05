385a68e5d9b5b4e3f53e8888272cd3d5b2ae01eb
maschloerning <schloegel.max@gmail.com>
2019-04-05 04:02:24 +0200

    added logging

--- final_project/my_sift.m
+++ final_project/my_sift.m
@@ -4,7 +4,7 @@ Parameters
 image : image (an mxnx3 array)
     the image in rgb format
 variant: string 
-    either 'keypoints' for keypoint sampling or 'dense' for dense sampling
+    either 'keypoint' for keypoint sampling or 'dense' for dense sampling
 sift_mode: string
     either 'gray', 'rgb' or 'opponent'
 
@@ -17,21 +17,21 @@ descriptors: 128xM-matrix
 function [descr] = my_sift(image, variant, sift_mode)
 % maybe use 'if strcmp(str1, str2)'?
 if variant == "dense"
-    [~, descr] =  vl_phow(single(squeeze(image)), 'Color', sift_mode)
+    [~, descr] =  vl_phow(single(squeeze(image)), 'Color', sift_mode, 'Step', 8);
     % Now we have to reshape the descriptor matrix from 384xm to 128x(mx3)
     if sift_mode == "rgb" | sift_mode == "opponent"
-        descr = cat(2,descr(1:128,:), descr(129:256,:), descr(257:284,:))
+        descr = cat(2,descr(1:128,:), descr(129:256,:), descr(257:384,:));
     end
     
 % Now we consider the case, where we only take keypoints. What is missing
 % is the case for keypoints and "rgb", or "opponent".
-elseif variant == "keypoints"
+elseif variant == "keypoint"
     if sift_mode == "gray"
-        [~, descr] = vl_sift(single(rgb2gray(squeeze(image))))
-    elsif sift_mode == "rgb"
+        [~, descr] = vl_sift(single(rgb2gray(squeeze(image))));
+    elseif sift_mode == "rgb"
         % [~, descr]
         % maybe reshaping necessary
-    elsif sift_mode == "opponent"
+    elseif sift_mode == "opponent"
         % [~, descr]
         % maybe reshaping necessary
     end
